e48ed9b94f77ec34fa78f280114bfc33a2ab9f7c
Alfredogomes <alfredommgomes@gmail.com>
2018-06-09 10:39:59 +0100

    encontra nomes

---
+++ findNames.py
@@ -0,0 +1,30 @@
+import fileinput, re, getopt, sys
+
+palMai = r"(?:\b[A-ZÀ-Ý]\w+|[A-ZÀ-Ý]\.)"
+preposicao = r"d[eao]s?"
+fimDeFrase = r"([^A-ZÀ-Ý][.!?]\s+|\n{2,}(?:-\s*)?)([A-ZÀ-Ý])"
+
+nomeProprio= palMai + "(?: (?:"+preposicao+" )?"+palMai+")*"
+
+pont = r"[.!?:,]"
+
+def findNames(numSub, sub, nomes):
+    lst = []
+    for line in sub:
+        line = line.split(" ")
+        for word in line:
+            if(re.findall(nomeProprio, word) != []):
+                newWord = re.split(pont,word)
+                if len(newWord) > 1: #se foi retirado pontuação
+                    lst.append(newWord[0])
+                else: lst.append(word)
+    if lst != []:
+        nomes.update({numSub : lst})
+    return nomes
+
+
+
+#_____Main______
+#subtitles = fileinput.input()  #ler o input
+#teste = "<i> Wake up, Dolores.</i>"
+#findNames(teste)
